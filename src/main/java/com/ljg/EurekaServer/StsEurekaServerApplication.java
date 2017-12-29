ebbb263b87472f5058b8a14a87f092601f120bec
JingangLi <ljg_emails@sina.com>
2017-12-29 18:11:28 +0100

    Add STS Eureka server project

---
+++ src/main/java/com/ljg/EurekaServer/StsEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.ljg.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsEurekaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ljg/EurekaServer/StsEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljg.EurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StsEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
